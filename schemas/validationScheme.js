2419a1a50d0570f3c5f66526f3c9bd89c451b2f5
Daniel Vasquez <daniel.vasquez.git@gmail.com>
2023-08-19 12:16:23 -0600

    Deploy API to FL0

---
+++ schemas/validationScheme.js
@@ -0,0 +1,45 @@
+const zod = require("zod");
+
+const movieSchema = zod.object({
+  title: zod.string({
+    invalid_type_error: "Movie title must be a string",
+    required_error: "Movie titel is required",
+  }),
+  year: zod.number().int().min(1900).max(2024),
+  director: zod.string(),
+  duration: zod.number().int().positive(),
+  poster: zod.string().url({
+    message: "Poster must be a valid URL",
+  }),
+  genre: zod.array(
+    zod.enum([
+      "Action",
+      "Adventure",
+      "Comedy",
+      "Drama",
+      "Fantasy",
+      "Horror",
+      "Biography",
+      "Crime",
+      "Romance",
+      "Sci-Fi",
+    ]), {
+      required_error: "Movie genre is required",
+      invalid_type_error: "Movie genre must be an array of enum Genre",
+    }
+  ),
+  rate: zod.number().min(0).max(10).default(5)
+});
+
+function validateMovie(object) {
+  return movieSchema.safeParse(object)
+}
+
+function validatePartialMovie(object) {
+  return movieSchema.partial().safeParse(object)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
